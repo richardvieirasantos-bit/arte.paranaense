5ed86600603655fe6509326649492da71f708b6a
richardvieirasantos-bit <richard.vieira.santos@escola.pr.gov.br>
2026-09-23 08:34:30 -0300

    atulizei o codigo

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const botao = document.querySelector("botaoMensagem");
+const botoesCurtir = document.querySelectorAll(.curtir);
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtir);
+    function curtir(){
+        const contador = botaoCurtir.querySelector("span");
+        if(curtiu === false){
+            contador.textContent++;
+            curtiu = true;
+        } else{
+            contador.textContent--;
+            curtiu = false;
+        }
 
-const mensagem = document.querySelector(#mensagemInterativa);
-
+    }
+});
